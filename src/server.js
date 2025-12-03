2eefd57fb4b581ea42a233f1c6f6909563d85242
Viktoriia Popel <xructa2009@gmail.com>
2025-12-03 22:41:44 +0100

    fix file server.js

--- src/server.js
+++ src/server.js
@@ -31,5 +31,5 @@ app.use(errorHandler);
 await connectMongoDB();
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
